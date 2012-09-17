af97c289eb01097cbb8644aea3895a2be1f6f8b1
Aaron Bloom <aaronlbloom@gmail.com>
2012-09-17 15:18:11 -0400

    First Commit
    Redoing DWA first commit

---
+++ p1/js/aaronlbloom.js
@@ -0,0 +1,18 @@
+<!-- Draggable box  from: http://jqueryui.com/demos/draggable/ , Dialog Box from http://jqueryui.com/demos/dialog/#animated-->
+				$(function() {
+					$( "#draggable" ).draggable();
+				});
+					// increase the default animation speed to exaggerate the effect
+					$.fx.speeds._default = 1000;
+					$(function() {
+						$( "#dialog" ).dialog({
+							autoOpen: false,
+							show: "blind",
+							hide: "explode"
+						});
+				
+						$( "#opener" ).click(function() {
+							$( "#dialog" ).dialog( "open" );
+							return false;
+						});
+					});
