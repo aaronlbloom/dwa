42724b6fad5bbbe653734012a5e9577e35b1c4eb
Aaron Bloom <aaronlbloom@gmail.com>
2012-12-03 21:03:18 -0500

    Add proposal controller to keep proposal

---
+++ p4.aaronlbloom.com/controllers/c_proposal.php
@@ -0,0 +1,21 @@
+<?php
+
+class proposal_controller extends base_controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+ public function index() {
+
+		$this->template->content = View::instance("v_proposal");
+		$client_files = Array(
+						 "../css/p4.css"
+	                    );
+				$this->template->client_files = Utils::load_client_files($client_files);   
+	      		
+		echo $this->template;
+
+	}
+   
+}
